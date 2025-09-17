1ec6a8e2342684fe35977e442924c439c77fb793
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-09-17 01:23:42 +0000

    ♻️	refactor -Instalacion de nativewind -Instalacion de tailwind -Creacion de babl.config.js e importacion de nativewind dentro de este -Creacion de global.css e importacion de tailwind dentro de este -Creacion de metro.config.js e importacion de configuracion de nativewind -Implementacion de classname en index como reemplazo de style

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import HelloWorld from "@/components/HelloWorld";
 import { View } from "react-native";
+import "../global.css"
+import { Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="bg-blue-500 flex-1 justify-center items-center  ">
+      <Text className=" text-3xl font-bold text-white"></Text>
       <HelloWorld></HelloWorld>
     </View>
   );
